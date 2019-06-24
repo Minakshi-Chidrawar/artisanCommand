2bb491fb3f1126a2811295e3acf86656ce967dd0
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-24 05:31:17 +0100

    Changed layout of display

--- resources/views/names/index.blade.php
+++ resources/views/names/index.blade.php
@@ -10,11 +10,52 @@
         </div>
     </div>
 
-    @foreach($names as $name)
+    <!-- @foreach($names as $name)
         <div class="row">
             <div class="col-2">
                 {{ $name }}
             </div>
         </div>
-    @endforeach
+    @endforeach -->
+
+    <!-- <div class="card-deck">
+        <div class="card">
+            <div class="card-body">
+                @foreach($names as $name)
+                    <div class="row">
+                        <div class="">
+                            {{ $name }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Select 5 Winners</button>
+
+        <div class="card">
+            <div class="card-body">
+            </div>
+        </div>
+    </div> -->
+
+    <div class="row">
+        <div class="col-4 rounded">
+            <ul class="list-unstyled pl-3">
+                @foreach($names as $name)
+                    <div class="row">
+                        <li>{{ $name }}</li>
+                    </div>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-4 text-center" style="margin-top:10%;">
+            <button type="submit" class="btn btn-primary">Select 5 Winners</button>
+        </div>
+
+        <div class="col-4 rounded" id ="winners">
+            <p>Test</p>
+        </div>
+    </div>
 @endsection
