66ec7c8a7b9b69ac55b8a910d3bad61c6fde4414
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-25 20:31:23 +0100

    Amended to add new name

--- resources/views/names/create.blade.php
+++ resources/views/names/create.blade.php
@@ -9,7 +9,7 @@
         </div>
     </div>
 
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-12">
             <form action="/names" method="POST">
                 <div class="form-group">
@@ -19,6 +19,21 @@
                 </div>
 
                 <button type="submit" class="btn btn-primary">Add Customer</button>
+            </form>
+        </div>
+    </div> -->
+
+    <div class="row">
+        <div class="col-6 mb-5">
+            <form action="/names" method="POST">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+                    <div>{{ $errors->first('name') }}</div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Add Customer</button>
+                @csrf
             </form>
         </div>
     </div>
