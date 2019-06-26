f832d7d380ac57188186ae145e299d30fa2ee3c0
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-26 20:24:48 +0100

    Split the layout

---
+++ resources/views/winnerButton.blade.php
@@ -0,0 +1,3 @@
+<div class="col-3 text-center" style="margin-top:10%;">
+    <button type="submit" class="btn btn-primary"  onclick="window.location='{{ route("names.show") }}'">Select 5 Winners</button>
+</div>
