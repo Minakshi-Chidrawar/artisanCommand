c2920e28b817d33aeae7e6fe94908165e32e4f5c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-26 20:23:10 +0100

    Split the layout

--- resources/views/names/index.blade.php
+++ resources/views/names/index.blade.php
@@ -11,19 +11,8 @@
     </div>
 
     <div class="row">
-        <div class="col-4 rounded">
-            <ul class="list-unstyled pl-3">
-                @foreach($names as $name)
-                    <div class="row">
-                        <li>{{ $name }}</li>
-                    </div>
-                @endforeach
-            </ul>
-        </div>
-
-        <div class="col-3 text-center" style="margin-top:10%;">
-            <button type="submit" class="btn btn-primary"  onclick="window.location='{{ route("names.show") }}'">Select 5 Winners</button>
-        </div>
+        @include('nameList')
+        @include('winnerButton')
 
         <div class="col-4 rounded" id ="winners">
             <h3>Winner List</h3>            
